a05c93949e417729027c7ced2a922c3259eb5176
Daniel Alves <devdanielprof@gmail.com>
2025-07-06 00:53:45 -0300

    Update UserRoblox.java

--- src/main/java/com/devdaniel/RobloxGetter/model/UserRoblox.java
+++ src/main/java/com/devdaniel/RobloxGetter/model/UserRoblox.java
@@ -1,18 +1,17 @@
 package com.devdaniel.RobloxGetter.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "user_roblox")
+@Data
 public class UserRoblox {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String username;
+    @Column
     private String password;
 }
