a3502c1339b29be78f6ffb900666f52750701a5d
devdanizin <devdanielprof@gmail.com>
2025-07-06 00:26:20 -0300

    added cors

---
+++ src/main/java/com/devdaniel/RobloxGetter/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.devdaniel.RobloxGetter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://roblox-41ub.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
